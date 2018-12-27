65c71697317bc5bc50964f40a96159e22d3ecaa5
oBusk <oscar.busk@silobreaker.com>
2018-12-27 13:58:07 +0100

    Add proof of problem

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const fs = require('fs');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
+const fileContent = fs.readFileSync('./FILE.txt');
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send(fileContent.toString());
 });
